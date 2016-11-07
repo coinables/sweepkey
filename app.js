89a462c01ae81ceb62b3a0c8c06950e61dd42a4d
Kyle <coinables@gmail.com>
2016-11-06 18:51:30 -0800

    spends all from first available input

--- app.js
+++ app.js
@@ -11,10 +11,48 @@ app.get("/", function(req, res){
 
 app.post("/address", function(req,res){
 	var wif = req.body.pkey;
+	var output = req.body.addy;
 	var address = new bitcore.PrivateKey(wif).toAddress();
 	console.log(address);
-	res.send("Addy " + address);
-});
+	
+	//create a tx
+	var privateKey = new bitcore.PrivateKey(wif);
+	
+	//get unspent from bcinfo
+	var request = require("request");
+	var url = "https://blockchain.info/unspent?active="+ address;
+	request({
+		url: url,
+		json: true
+	},function(error, response, body){
+		var num = body.unspent_outputs.length;
+		
+		
+		var utxo = {
+			"txId": body.unspent_outputs[0].tx_hash_big_endian,
+			"outputIndex": body.unspent_outputs[0].tx_output_n,
+			"address": address,
+			"script": body.unspent_outputs[0].script,
+			"satoshis": body.unspent_outputs[0].value
+		};
+		
+		var transaction = new bitcore.Transaction()
+		  .from(utxo)
+		  .to('1Gokm82v6DmtwKEB8AiVhm82hyFSsEvBDK', body.unspent_outputs[0].value)
+		  .sign(wif);
+		
+		//console.log(i);
+		//console.log(body.unspent_outputs[i].tx_hash_big_endian);
+		//console.log(body.unspent_outputs[i].value);
+	
+		
+		console.log(transaction);
+		console.log("done");
+	})
+	
+	//display to user
+		res.send("Addy " + address);
+	});
 
 app.listen(8080, function(){
 	console.log("sever running on 8080");
