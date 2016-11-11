4c0da457a8a8b76541ce86b78586e955961b9510
Kyle <coinables@gmail.com>
2016-11-10 18:18:03 -0800

    no utxo bug

--- app.js
+++ app.js
@@ -37,62 +37,64 @@ app.post("/address", function(req,res){
 				url: url,
 				json: true
 			},function(error, response, body){
-				var num = body.unspent_outputs.length;
-				var utxos = [];
-				var totalSats = 0;	
-				var txSize = 44;
-					
-					for(i=0;i < num; i++){
-					var utxo = {
-						"txId": body.unspent_outputs[i].tx_hash_big_endian,
-						"outputIndex": body.unspent_outputs[i].tx_output_n,
-						"address": address,
-						"script": body.unspent_outputs[i].script,
-						"satoshis": body.unspent_outputs[i].value
-					};
-					utxos.push(utxo);
-					totalSats = totalSats + body.unspent_outputs[i].value;
-					txSize = txSize + 180;
-					};
-					
-				var fee = txSize * 20;
-				totalSats = totalSats - fee;
-				
-				if(totalSats < 1){
-				 alert("you don't have enough funds to send with a sufficient fee");
-				} else {
-					
-				var transaction = new bitcore.Transaction()
-				  .from(utxos)
-				  .to(output, totalSats)
-				  .sign(pkeyValue);
-				
+				if(body.unspent_outputs){
+					var num = body.unspent_outputs.length;
+					var utxos = [];
+					var totalSats = 0;	
+					var txSize = 44;
 						
-				
-				var txjson = transaction.toString();
-				var pload = {
-					"tx_hex": txjson
-				};
-				
-				request({
-					url: "https://chain.so/api/v2/send_tx/BTC/",
-					method: "POST",
-					json: true,
-					headers: {
-						"content-type": "application/json",
-					},
-					body: pload
-				}, function(err, response){
-					if(err){ 
-						return console.log(err);
+						for(i=0;i < num; i++){
+						var utxo = {
+							"txId": body.unspent_outputs[i].tx_hash_big_endian,
+							"outputIndex": body.unspent_outputs[i].tx_output_n,
+							"address": address,
+							"script": body.unspent_outputs[i].script,
+							"satoshis": body.unspent_outputs[i].value
+						};
+						utxos.push(utxo);
+						totalSats = totalSats + body.unspent_outputs[i].value;
+						txSize = txSize + 180;
+						};
+						
+					var fee = txSize * 20;
+					totalSats = totalSats - fee;
+					
+					if(totalSats < 1){
+					 alert("you don't have enough funds to send with a sufficient fee");
+					} else {
+						
+					var transaction = new bitcore.Transaction()
+					  .from(utxos)
+					  .to(output, totalSats)
+					  .sign(pkeyValue);
+					
+							
+					
+					var txjson = transaction.toString();
+					var pload = {
+						"tx_hex": txjson
 					};
-					console.log(JSON.stringify(response));
-				});
-				console.log("done");
-				};
-				//display to user
-				res.send("<body bgcolor='#0b161d'><font color='#f8f8f8'><br>Destination: <a href='https://btc.com/"+output+"' target='_blank'>" + output + "</a><br>Amount sent: " + totalSats + 
-				"<br>Fee: " + fee + "<br><br><a href='../'>Send Another</a></font>");
+					
+					request({
+						url: "https://chain.so/api/v2/send_tx/BTC/",
+						method: "POST",
+						json: true,
+						headers: {
+							"content-type": "application/json",
+						},
+						body: pload
+					}, function(err, response){
+						if(err){ 
+							return console.log(err);
+						};
+						console.log(JSON.stringify(response));
+					});
+					console.log("done");
+					};
+					//display to user
+					res.send("<body bgcolor='#0b161d'><font color='#f8f8f8'><br>Destination: <a href='https://btc.com/"+output+"' target='_blank'>" + output + "</a><br>Amount sent: " + totalSats + 
+					"<br>Fee: " + fee + "<br><br><a href='../'>Send Another</a></font>");
+				}
 			});
 		
 		} else {
